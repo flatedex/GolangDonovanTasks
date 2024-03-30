9358ddcf8fdab53419e76dab868355032978a8da
flatedex <flatedex@mail.ru>
2024-03-30 22:36:31 +0300

    chore: chapter 1 task 1.4

--- task2dup.go
+++ task2dup.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,13 +14,15 @@ func main() {
 
 func dup() {
 	//dup1()
-	dup2()
+	//dup2()
+	modifiedDup2()
+	//dup3()
 }
 
 func dup1() {
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
-	
+
 	for input.Scan() {
 		counts[input.Text()]++
 	}
@@ -51,8 +55,52 @@ func dup2() {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
+}
+func modifiedDup2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			valve := false
+			for _, n := range counts {
+				if n > 1 {
+					valve = true
+					break
+				}
+			}
+			if valve == true {
+				fmt.Printf(arg);
+			}
+			f.Close()
+		}
+	}
+}
 
-	
+func dup3() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
 
 func countLines(f *os.File, counts map[string]int) {
